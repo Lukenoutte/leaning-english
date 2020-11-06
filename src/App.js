4367db8f735b0054c61e80cfcd679ea04580ba85
Lucas Lima <lucas.alima@souunit.com.br>
2020-11-06 10:52:10 -0300

    translations

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { useState, useRef, useEffect } from "react";
 import "./styles/global.css";
 import "./styles/app.css";
 
-
 function App() {
   const [phrase, setPhrase] = useState([]);
   const [unknownWords, setUnknownWords] = useState([]);
+  const [translatedWords, setTranslatedWords] = useState([]);
   const inputPhrase = useRef(null);
-  const uuidv4 = require('uuid/v4');
-  const axios = require('axios').default;  
-
-
+  const uuidv4 = require("uuid/v4");
+  const axios = require("axios").default;
 
   function handleClickInput() {
     let currentPhrase = inputPhrase.current.value;
@@ -23,30 +21,43 @@ function App() {
     }
   }
 
+  function listHightLightedWords() {
+    let divList = [];
+    for (let i = 0; i < unknownWords.length; i++) {
+      if(translatedWords[i] !== undefined){
+      divList.push(<div className="unknown-words" key={i}>
+      <p key={i}>{unknownWords[i] + "-" + translatedWords[i]}</p>
+      </div>);
+      }
+     }
+     return divList;
+  }
+
   useEffect(() => {
-    axios({
-      baseURL: process.env.REACT_APP_ENDPOINT,
-      url: '/translate',
-      method: 'post',
-      headers: {
-          'Ocp-Apim-Subscription-Key': process.env.REACT_APP_KEY,
-          'Content-type': 'application/json',
-          'X-ClientTraceId': uuidv4().toString()
-      },
-      params: {
-          'api-version': '3.0',
-          'from': 'en',
-          'to': ['pt']
-      },
-      data: [{
-          'text': 'Hello World!'
-      }],
-      responseType: 'json'
-  }).then(function(response){
-      console.log(JSON.stringify(response.data, null, 4));
-  })
-  
-  }, []);
+    if (unknownWords.length > 0) {
+      axios({
+        baseURL: process.env.REACT_APP_ENDPOINT,
+        url: "/translate",
+        method: "post",
+        headers: {
+          "Ocp-Apim-Subscription-Key": process.env.REACT_APP_KEY,
+          "Content-type": "application/json",
+          "X-ClientTraceId": uuidv4().toString(),
+        },
+        params: {
+          "api-version": "3.0",
+          from: "en",
+          to: ["pt"],
+        },
+        data: [{ text: unknownWords[unknownWords.length - 1] }],
+        responseType: "json",
+      }).then(function (response) {
+        let newTranslation = response.data[0].translations[0].text;
+        setTranslatedWords((oldArray) => [...oldArray, newTranslation]);
+      });
+    }
+
+  }, [axios, uuidv4, unknownWords]);
 
   return (
     <div className="app">
@@ -75,13 +86,8 @@ function App() {
         </div>
         <div className="unknown-words-container">
           {unknownWords.length > 0 &&
-            unknownWords.map((word, index) => {
-              return (
-                <div className="unknown-words" key={index}>
-                  <p key={index}>{word}</p>
-                </div>
-              );
-            })}
+            listHightLightedWords().map(div => { return div})
+            }
         </div>
       </div>
     </div>
