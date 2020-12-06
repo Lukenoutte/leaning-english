a933f86458d1efb4030fed12c109bcc3ef578b08
Lucas Lima <lucas.alima@souunit.com.br>
2020-12-05 23:15:00 -0300

    empty input

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,37 +1,69 @@
-import React,{ useRef } from "react";
+import React, { useRef, useState } from "react";
 import HeaderAndFotter from "../components/headerAndFooter";
 import "../styles/login.css";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import services from "../services";
 
 function Login() {
-  const inputEmail = useRef('');
-  const inputPass = useRef('');
-
+  const inputEmail = useRef("");
+  const inputPass = useRef("");
+  const [emptyInput, setEmptyInput] = useState(false);
 
   const HandleLogin = (event) => {
     event.preventDefault();
     let email = inputEmail.current.value;
     let pass = inputPass.current.value;
 
-    if (email !== "") {
-     
-      services.login({email: email, pass: pass});
+    if (email !== "" && pass !== "") {
+      services.login({ email: email, pass: pass });
+      setEmptyInput(false);
+    } else {
+      
+      setEmptyInput(true);
     }
-  
   };
+
 
   return (
     <HeaderAndFotter>
       <div className="login global-wrapper">
         <div className="center-container">
-          
-          <div className="inputs-wrapper shadow-light styled-buttons"> 
-          <h1>Login</h1>
-          <input type="text" placeholder="E-mail" ref={inputEmail}/>
-          <input id="pass-input" type="password" placeholder="Password" ref={inputPass}/>
-          <Link to="/">Forgot password?</Link>
-          <button onClick={(e) => HandleLogin(e)} >Login</button>
+          <div className="inputs-wrapper shadow-light styled-buttons">
+
+          {emptyInput ? (
+              <div className="empty-input-error">
+                      <p>Ops, Campo vazio!</p>
+              </div>
+            ) : (
+              false
+            )}
+
+            <h1>Login</h1>
+
+            <input
+              type="text"
+              placeholder="E-mail"
+              ref={inputEmail}
+              className={
+                emptyInput && inputEmail.current.value === ""
+                  ? "input-error"
+                  : ""
+              }
+            />
+
+            <input
+              id="pass-input"
+              type="password"
+              placeholder="Password"
+              ref={inputPass}
+              className={
+                emptyInput && inputPass.current.value === ""
+                  ? "input-error"
+                  : ""
+              }
+            />
+            <Link to="/">Forgot password?</Link>
+            <button onClick={(e) => HandleLogin(e)}>Login</button>
           </div>
         </div>
       </div>
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,21 +1,88 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import HeaderAndFotter from "../components/headerAndFooter";
 import "../styles/sign_up.css";
 
+import services from "../services";
 
-function CreateAccount() {
+function SignUp() {
+  const inputName = useRef("");
+  const inputEmail = useRef("");
+  const inputPass = useRef("");
+  const inputConfirmPass = useRef("");
+  const [emptyInput, setEmptyInput] = useState(false);
+
+  const HandleSignUp = (event) => {
+    event.preventDefault();
+    let name = inputName.current.value;
+    let email = inputEmail.current.value;
+    let pass = inputPass.current.value;
+    let confirmPass = inputConfirmPass.current.value;
+
+    if (name === "" || email === "" || pass === "" || confirmPass === "") {
+      setEmptyInput(true);
+    } else {
+      setEmptyInput(false);
+    }
+
+    if (pass !== confirmPass) {
+      return;
+    }
+  };
+
   return (
     <HeaderAndFotter>
       <div className="sign-up global-wrapper">
         <div className="center-container">
-          
-          <div className="inputs-wrapper shadow-light styled-buttons"> 
-          <h1>Sign Up</h1>
-          <input type="text" placeholder="Name"/>
-          <input type="text" placeholder="E-mail"/>
-          <input type="password" placeholder="Password"/>
-          <input  type="password" placeholder="Confirm Password"/>
-          <button>Start</button>
+          <div className="inputs-wrapper shadow-light styled-buttons">
+            {emptyInput ? (
+              <div className="empty-input-error">
+                <p>Ops, Campo vazio!</p>
+              </div>
+            ) : (
+              false
+            )}
+            <h1>Sign Up</h1>
+            <input
+              type="text"
+              placeholder="Name"
+              ref={inputName}
+              className={
+                emptyInput && inputName.current.value === ""
+                  ? "input-error"
+                  : ""
+              }
+            />
+            <input
+              type="text"
+              placeholder="E-mail"
+              ref={inputEmail}
+              className={
+                emptyInput && inputEmail.current.value === ""
+                  ? "input-error"
+                  : ""
+              }
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              ref={inputPass}
+              className={
+                emptyInput && inputPass.current.value === ""
+                  ? "input-error"
+                  : ""
+              }
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              ref={inputConfirmPass}
+              className={
+                emptyInput && inputConfirmPass.current.value === ""
+                  ? "input-error"
+                  : ""
+              }
+            />
+            <button onClick={(e) => HandleSignUp(e)}>Start</button>
           </div>
         </div>
       </div>
@@ -23,4 +90,4 @@ function CreateAccount() {
   );
 }
 
-export default CreateAccount;
+export default SignUp;
