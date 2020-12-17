42f7559199fd1666bf679c15a14481847d8f93fc
Lucas Lima <lucas.alima@souunit.com.br>
2020-12-16 22:58:51 -0300

    drawer

--- src/components/headerAndFooter.js
+++ src/components/headerAndFooter.js
@@ -1,47 +1,76 @@
-import React, {useContext} from "react";
+import React, { useContext, useState } from "react";
 import "../styles/header_and_footer.css";
 import { ReactComponent as MainIcon } from "../assets/icons/mainIcon.svg";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Context } from "../context/AuthContext";
+import { Drawer } from "@material-ui/core";
 
-function HeaderAndFooter(props) { 
-    const { authenticated, handleLogout } = useContext(Context);
 
-    return(
-      <>
+function HeaderAndFooter(props) {
+  const { authenticated, handleLogout } = useContext(Context);
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+
+  const handleDrawer = () => {
+    if (drawerIsOpen) {
+      setDrawerIsOpen(false);
+    } else {
+      setDrawerIsOpen(true);
+    }
+  };
+
+  return (
+    <>
       <header className="shadow-light">
-      
         <div className="center-container-header icon-and-name">
           <Link to="/">
-          <MainIcon className="main-icon" /> Surligner
+            <MainIcon className="main-icon" /> Surligner
           </Link>
-          {!authenticated?(
-          <div className="login-and-sign-up">
-          <Link to="/login">Login</Link>
-          <Link to="/sign_up">Sign Up</Link>
-          </div>): (<button onClick={handleLogout}> Logout </button>)
-          }
+
+          <button
+            onClick={handleDrawer}
+          >
+            Drawer
+          </button>
+          
+          <Drawer
+            anchor={"top"}
+            open={drawerIsOpen}
+            onEscapeKeyDown={handleDrawer}
+            onBackdropClick={handleDrawer}
+          >
+            <a href="/">aaaa</a>
+            <a href="/">aaaa</a>
+            <a href="/">aaaa</a>
+            <a href="/">aaaa</a>
+          </Drawer>
+       
+          {!authenticated ? (
+            <div className="login-and-sign-up">
+              <Link to="/login">Login</Link>
+              <Link to="/sign_up">Sign Up</Link>
+            </div>
+          ) : (
+            <button onClick={handleLogout}> Logout </button>
+          )}
         </div>
       </header>
-      <main>
-        {props.children}
-      </main>
+      <main>{props.children}</main>
       <footer className="shadow-light">
-     
         <p>
-        Developed by{' '}
+          Developed by{" "}
           <a
             target="_blank"
             rel="noopener noreferrer"
             title="Github"
             href="https://github.com/Lukenoutte"
           >
-             @Lukenoutte
+            @Lukenoutte
           </a>
         </p>
       </footer>
     </>
-    );
+  );
 }
 
 export default HeaderAndFooter;
