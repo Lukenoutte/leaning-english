8e9c0c83c25e9223503d922e579b1f9886c4a61a
Lucas Lima <lucas.alima@souunit.com.br>
2021-01-14 21:39:51 -0300

    fix popup mobile

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,22 +1,19 @@
-import React,{ useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import "./styles/pop_up.css";
 import { MainContext } from "../context/MainContext";
 
 export default function PopUp(props) {
-    const {
-        showPopUp,
-        setShowPopUp
-      } = useContext(MainContext);
-    
-      useEffect(() => {
-    
-        if(showPopUp){
-            setTimeout(() => {setShowPopUp(false)}, 2000);
-        }
-      }, [showPopUp, setShowPopUp]);
-    
+  const { showPopUp, setShowPopUp } = useContext(MainContext);
 
-    return(<div className="pop-up-wrapper">        
-        {props.message}
-    </div>);
+  useEffect(() => {
+    if (showPopUp) {
+      setTimeout(() => {setShowPopUp(false)}, 2000);
+    }
+  }, [showPopUp, setShowPopUp]);
+
+  return (
+    <div className="pop-up-wrapper">
+      <div className="pop-up">{props.message}</div>
+    </div>
+  );
 }
