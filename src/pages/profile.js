bc4ad986ea884c90892b667b7f054a0b399c2eef
Lucas Lima <lucas.alima@souunit.com.br>
2021-01-18 23:34:06 -0300

    profile listing words

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -4,36 +4,59 @@ import { userInformations } from "../services";
 import { getWords } from "../services";
 import { MainContext } from "../context/MainContext";
 import WordContainer from "../components/wordContainer";
+import { AuthContext } from "../context/AuthContext";
+import Loading from "../components/loading";
+import "../styles/profile.css";
 
 export default function Profile() {
   const [userInfo, setUserInfo] = useState([]);
   const { setProfileWordsList, profileWordsList } = useContext(MainContext);
+  const { authenticated } = useContext(AuthContext);
+
+  function listWordsFromProfile() {
+    let divList = [];
+    profileWordsList.map((word) =>
+      divList.push(
+        <WordContainer isProfileWord={false} key={word}>          
+          <p>{word.toUpperCase()}</p>
+        </WordContainer>
+      )
+    );
+
+    return divList;
+  }
 
   useEffect(() => {
-    async function getUserInfo() {
-      setUserInfo(await userInformations());
+    if (authenticated) {
+      async function getUserInfo() {
+        setUserInfo(await userInformations());
+      }
+      getUserInfo();
     }
-    getUserInfo();
-  }, []);
+  }, [authenticated]);
 
   useEffect(() => {
     // Get unknown words profile list from my api
-    const wordsList = async () => {
-      const words = await getWords();
-      if (words.status && words.status === 200) {
-        setProfileWordsList(words.data);
-        console.log(words.data);
-      }
-    };
-    wordsList();
-  }, [setProfileWordsList]);
+    if (authenticated) {
+      const wordsList = async () => {
+        const words = await getWords();
+        if (words.status && words.status === 200) {
+          setProfileWordsList(words.data);
+        }
+      };
+      wordsList();
+    }
+  }, [setProfileWordsList, authenticated]);
 
   return (
     <HeaderAndFotter>
-      <div className="main-tool global-wrapper">
+      <div className="profile global-wrapper">
         <div className="center-container">
-          {console.log(profileWordsList)}
-          <WordContainer isProfileWord={false}> Test </WordContainer>
+         
+          {profileWordsList && profileWordsList.length > 0?listWordsFromProfile().map((div) => {
+            return div;
+          }):(<Loading/>)}
+        
         </div>
       </div>
     </HeaderAndFotter>
